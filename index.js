29cf9a5ee2782e6c460fa8ecee45bf6c7a8dfdcd
Fi_emu <saemu283@gmail.com>
2024-08-01 00:59:30 +0600

    second commit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const cors = require('express');
+const cors = require('cors');
 const app = express();
 require('dotenv').config();
 const port = process.env.port || 1101;
@@ -10,7 +10,7 @@ app.use(express.json())
 
 
 // mongoDB connection
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xphzfyn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 const client = new MongoClient(uri, {
   serverApi: {
@@ -23,11 +23,70 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+
+    const servicesCollection = client.db('carsDoctor').collection('services');
+    const checkOutCollection = client.db('carsDoctor').collection('checkOuts');
+
+    // servicesCollection
+    app.get('/services',async(req,res)=>{
+        const cursor = servicesCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+    })
+
+    app.get('/services/:id',async(req,res)=>{
+        const id = req.params.id;
+        const query = {
+            _id : new ObjectId(id)
+        };
+        const result = await servicesCollection.findOne(query);
+        res.send(result)
+    })
+
+    // checkOutCollection
+    app.post('/checkOuts',async(req,res)=>{
+        const checkOutInfo = req.body;
+        const result = await checkOutCollection.insertOne(checkOutInfo);
+        res.send(result)
+    })
+
+    app.get('/checkOuts',async(req,res)=>{
+        let query = {};
+        if(req.query?.email){
+            query = {email:req.query.email}
+        }
+        const result = await checkOutCollection.find(query).toArray();
+        res.send(result)
+    })
+
+    app.delete('/checkOuts/:id',async(req,res)=>{
+        const id = req.params.id;
+        const query = {
+            _id: new ObjectId(id)
+        };
+        const result = await checkOutCollection.deleteOne(query);
+        res.send(result)
+    })
+
+    app.patch('/checkOuts/:id',async(req,res)=>{
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)};
+        const updateCheckOut = req.body;
+        const updateDocs ={
+            $set:{
+                status: updateCheckOut.status
+            }
+        }
+        const result = await checkOutCollection.updateOne(query,updateDocs);
+        res.send(result)
+    })
+
     await client.connect();
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
   } finally {
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
